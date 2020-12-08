ff11e0291ac8b53ad0222d3385041f1254aec8aa
Judith Chuan Ti Chen <chen.chuan@husky.neu.edu>
2020-12-07 17:05:22 -0800

    Created SearchBar and reducers for filters

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './users_reducer';
+import filter from './filters_reducer';
 //e.g. import favoritePet from './favoritePet_reducer';
 const rootReducer = combineReducers({
-    user
+    user,
+    filter
     //favoritePet
 });
 
